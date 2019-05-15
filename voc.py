92494e154c1b9676d14eaaa6f0e91417dd3828c3
nivedha1998 <50647616+nivedha1998@users.noreply.github.com>
2019-05-15 10:34:51 +0530

    Update voc.py

--- voc.py
+++ voc.py
@@ -1,5 +1,7 @@
 ch=input("enter a character")
 if(ch=='A' or ch=='E' or ch=='I'or ch=='O' or ch=='U' or ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u'):
  print("Vowel")
- else:
+ elif:
   print("Consonant")
+  else:
+   print("invalid")
