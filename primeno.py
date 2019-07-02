e6ea635544566a7324ba76d8faacd66d83ded43c
nivedha1998 <50647616+nivedha1998@users.noreply.github.com>
2019-07-02 23:45:50 +0530

    Update primeno.py

--- primeno.py
+++ primeno.py
@@ -2,8 +2,8 @@ n = int(input(""))
 if n > 1:
     for i in range(2, n // 2):
       if (n % i) == 0:
-       print(n,"not a prime number")
+       print(n," no not a prime number")
       else:
-       print(n,"is a prime number")
+       print(n,"yes it is a prime number")
 else:
- print(n,"not a prime number")
+ print(n," no not a prime number")
