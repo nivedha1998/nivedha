8533ed0897a31f1317d35fb4537802b08431cdb9
nivedha1998 <50647616+nivedha1998@users.noreply.github.com>
2019-07-04 00:55:13 +0530

    Create hunt10.py

---
+++ hunt10.py
@@ -0,0 +1,13 @@
+nm=input().split()
+A=input().split()
+B=input().split()
+A.sort()
+B.sort()
+c=0
+for i in B:
+    if i in A:
+        c=c+1
+if c==len(B):
+    print('Yes')
+else:
+    print('No')
