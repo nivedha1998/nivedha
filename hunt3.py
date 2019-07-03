d918c8e8d650789effddea2e3dc9ce13dd2b4134
nivedha1998 <50647616+nivedha1998@users.noreply.github.com>
2019-07-04 00:46:52 +0530

    Create hunt3.py

---
+++ hunt3.py
@@ -0,0 +1,11 @@
+n=int(input(""))
+n1=[int(x) for x in input().split()[:n]]
+c=[]
+for i in range(0,int(len(n1))):
+    if(i==n1[i]):
+        c.append(i)
+if(int(len(c))!=0):
+    for g in c:
+        print(g,end=" ")
+else:
+    print(-1)
