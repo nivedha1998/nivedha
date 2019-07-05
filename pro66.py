4802f359aef0f08d0e96e64962248b9a719844a2
nivedha1998 <50647616+nivedha1998@users.noreply.github.com>
2019-07-06 00:46:39 +0530

    Update pro66.py

--- pro66.py
+++ pro66.py
@@ -1,5 +1,5 @@
 a, b, f, k = map(int, input().split())
-cnt = 0
+c = 0
 d = b-f
 if (d >= 0):
     s = (a-f)*2
@@ -8,12 +8,12 @@ if (d >= 0):
              s =s/ 2
         if (d < s):
             d= b
-            cnt += 1
+            c += 1
         d = d - s
         if (d < 0):
-            cnt = -1
+            c = -1
             break
         s = 2*a - s
-    print (cnt)
+    print (c)
 else:
     print (-1)
