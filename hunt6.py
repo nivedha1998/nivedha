96e929be7756ffd8e1cd0218da1a35ae4d167d92
nivedha1998 <50647616+nivedha1998@users.noreply.github.com>
2019-07-06 00:09:54 +0530

    Create hunt6.py

---
+++ hunt6.py
@@ -0,0 +1,14 @@
+n1=int(input())
+l=list(map(int,input().split()))
+l.sort()
+r=[]
+for i in range(len(l)-1):
+    if l[i]==l[i+1]:
+        r.append(l[i])
+
+if r:
+    for j in set(r):
+        print(j,end=" ")
+        break
+else:
+    print("unique")
