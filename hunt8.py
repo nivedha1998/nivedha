a8994c2c42831c43c9d1e956959197b3379eb384
nivedha1998 <50647616+nivedha1998@users.noreply.github.com>
2019-07-06 00:15:42 +0530

    Update hunt8.py

--- hunt8.py
+++ hunt8.py
@@ -1,7 +1,7 @@
-n1=int(input())
-n2=list(map(int,input().split()))
-for i in range(0,n1-2):
- for j in range(i+1,n1-1):
-  for k in range(j+1,n1):
-   if(n2[i]+n2[j]==n2[k]):
-    print(n2[i], n2[j], n2[k])
+N1=int(input())
+N2=list(map(int,input().split()))
+for i in range(0,N1-2):
+ for j in range(i+1,N1-1):
+  for k in range(j+1,N1):
+   if(N2[i]+N2[j]==N2[k]):
+    print(N2[i], N2[j], N2[k])
