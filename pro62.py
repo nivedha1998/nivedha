1f0f56407217dcad29c19acd0e823e316b285c6d
nivedha1998 <50647616+nivedha1998@users.noreply.github.com>
2019-07-06 00:35:12 +0530

    Update pro62.py

--- pro62.py
+++ pro62.py
@@ -1,12 +1,10 @@
-str1=input()
-ld=""
-c=0
-l1=[]
-if str1==str1[::-1]:
-    l1.append(0)
-for i in range(0,len(str1)-1):
-    for j in range(i+1,len(str1)):
-        ld=str1[i:j+1]
-        if ld==ld[::-1]:
-            l1.append(len(str1)-len(ld))
-print(min(l1))
+n=input()
+b=0
+c=[]
+for i in range(0,len(n)-1):
+    for j in range(i+1,len(n)):
+        k=n[i:j+1]
+        l=k[::-1]
+        if k==l:
+            c.append(len(n)-len(l))
+print(min(c))
