574c35f7a6604364295d46ec458da3cfbe08aad2
nivedha1998 <50647616+nivedha1998@users.noreply.github.com>
2019-07-06 00:06:57 +0530

    Update hunt5.py

--- hunt5.py
+++ hunt5.py
@@ -1,11 +1,11 @@
 n=input()
-o=1
+k=1
 for j in range(len(n)-1):
     s=n[j]+n[j+1]
     p=int(s)
     if p<=26 and n[j]!="0":
-        o+=1
-if o==3:
-    print(o)
+        k+=1
+if k==3:
+    print(k)
 else:
-    print(o+(o-1)//2)
+    print(k+(k-1)//2)
