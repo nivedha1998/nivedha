ff91de3ec71dff70b4b3bafa95ded19048299128
nivedha1998 <50647616+nivedha1998@users.noreply.github.com>
2019-07-06 00:18:55 +0530

    Update hunt9.py

--- hunt9.py
+++ hunt9.py
@@ -1,11 +1,11 @@
 N=int(input(""))
-n=list(map(int,input().split()))
-o=len(n)
-large=max(n)
+n1=list(map(int,input().split()))
+o=len(n1)
+large=max(n1)
 y,z=0,0
 for i in range(0,o-1):
  for j in range(i+1,o):
-  if abs(n[i]+n[j])< large:
-   y,z=n[i],n[j]
+  if abs(n1[i]+n1[j])< large:
+   y,z=n1[i],n1[j]
    large=abs(y+z)
 print(y, z)
