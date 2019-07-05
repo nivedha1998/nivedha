21c69c2d5c1eee2a51a9983a51361b71ed120b84
nivedha1998 <50647616+nivedha1998@users.noreply.github.com>
2019-07-06 01:18:11 +0530

    Create P51.py

---
+++ P51.py
@@ -0,0 +1,17 @@
+n1=int(input())
+li=list(map(int,input().split()))
+n2=[]
+n3=1
+for i in range(n1):
+  v=li[i:]
+  ans=len(v)
+  for j in range(ans-1):
+    if v[j]>0 and v[j+1]<0:
+      n3=n3+1
+    elif v[j]<0 and v[j+1]>0:
+      n3=n3+1
+    else:
+      break
+  n2.append(str(n3))
+  n3=1
+print(" ".join(n2))
