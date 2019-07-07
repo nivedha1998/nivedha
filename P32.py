5eeb2a3425e0da0d8b1b360237576d0c19dcef32
nivedha1998 <50647616+nivedha1998@users.noreply.github.com>
2019-07-07 23:04:05 +0530

    Update P32.py

--- P32.py
+++ P32.py
@@ -1,19 +1,19 @@
 a,b=map(int,input().split())
 if a>b:
     a,b=b,a
-k=[]
+ki=[]
 for i in range(a):
     lis=list(map(int,input().split()))
     lis.sort()
-    k.append(lis)
+    ki.append(lis)
 for j in range(0,b):
     p=[]
     for i in range(0,a):
-        p.append(k[i][j])
+        p.append(ki[i][j])
     p.sort()
     r=0
     for i in range(0,a):
-        k[i][j]=p[r]
+        ki[i][j]=p[r]
         r=r+1
-for i in k:
+for i in ki:
     print(*i)
