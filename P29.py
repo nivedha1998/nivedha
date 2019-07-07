48f74ba8e692effdc8ec381f3fa1e2c75336a149
nivedha1998 <50647616+nivedha1998@users.noreply.github.com>
2019-07-07 22:12:21 +0530

    Update P29.py

--- P29.py
+++ P29.py
@@ -1,15 +1,15 @@
-a=int(input())
-i=0
-x=0
-b=[]
-while i<90 and i<a:
-  r=0
-  for j in str(a-i):
-    r+=int(j)
-  if r+(a-i)==a:
-    x+=1
-    b.append(a-i)
-  i+=1
-print(x)
-for i in b:
-  print(i)
+vj=int(input())
+sk=0
+rs=0
+m=[]
+while sk<90 and sk<vj:
+  s=0
+  for k in str(vj-sk):
+    s+=int(k)
+  if s+(vj-sk)==vj:
+    rs+=1
+    m.append(vj-sk)
+  sk+=1
+print(rs)
+for sk in m:
+  print(sk)
