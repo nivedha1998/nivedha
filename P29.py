883b0d459710106ec81273e08e9f7f21125716c8
nivedha1998 <50647616+nivedha1998@users.noreply.github.com>
2019-07-07 22:13:25 +0530

    Update P29.py

--- P29.py
+++ P29.py
@@ -1,14 +1,14 @@
-vj=int(input())
+v=int(input())
 sk=0
 rs=0
 m=[]
-while sk<90 and sk<vj:
+while sk<90 and sk<v:
   s=0
-  for k in str(vj-sk):
+  for k in str(v-sk):
     s+=int(k)
-  if s+(vj-sk)==vj:
+  if s+(v-sk)==v:
     rs+=1
-    m.append(vj-sk)
+    m.append(v-sk)
   sk+=1
 print(rs)
 for sk in m:
