5cecc07e547375f35705cc5efdc20c39844bf9c8
nivedha1998 <50647616+nivedha1998@users.noreply.github.com>
2019-07-07 20:54:23 +0530

    Update P12.py

--- P12.py
+++ P12.py
@@ -1,11 +1,11 @@
 a1,b1=map(int,input().split())
 lis=list(map(int,input().split()))
-l=[]
+l1=[]
 for i in range(0,b1):
      u1,v1=map(int,input().split())
-     l.append([u1,v1])
+     l1.append([u1,v1])
 for i in range(b1):
-     lower=l[i][0]
-     upper=l[i][1]
+     lower=l1[i][0]
+     upper=l1[i][1]
      s=sum(lis[lower-1:upper])
      print(s)
