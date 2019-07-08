0c0448b629c89954586061bd5d65cee936d5422e
nivedha1998 <50647616+nivedha1998@users.noreply.github.com>
2019-07-08 21:02:13 +0530

    Create H21.py

---
+++ H21.py
@@ -0,0 +1,18 @@
+N,K=map(int,input().split())
+xx=[]
+l1=[]
+for i in range(N):
+    l=[int(xx) for xx in input().split()]
+    xx.append(l)
+    if 0 in l:
+        m=l.index(0)
+        l1.append(m)
+for i in range(len(xx)):
+    if 0 in xx[i]:
+        for j in range(K):
+            xx[i][j]=0
+for i in l1:
+    for j in range(N):
+        xx[j][i]=0
+for i in xx:
+    print(*i)
