1df50fb5d4779f1947871ea8791de828a5f758e4
nivedha1998 <50647616+nivedha1998@users.noreply.github.com>
2019-07-09 23:11:50 +0530

    Create H91.py

---
+++ H91.py
@@ -0,0 +1,12 @@
+s=input()
+n=1
+if len(s)==1:
+    print("yes")
+else:
+    for j in s:
+        if s.count(j)==len(s):
+            print("no")
+            n=0
+            break
+    if n==1:
+        print("yes")
