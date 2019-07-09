13cb5c3a4b1fb8050bef7280d22262a823b8fd10
nivedha1998 <50647616+nivedha1998@users.noreply.github.com>
2019-07-09 23:19:13 +0530

    Update H92.py

--- H92.py
+++ H92.py
@@ -1,28 +1,28 @@
 from itertools import permutations
-s=input()
-k=permutations(s)
+N=input()
+k=permutations(N)
 l=[]
 m=(-1)
 a="abcdefghijklmnopqrstuvwxyz"
-if a==s:
-  print(s)
-elif s==a[::-1]:
+if a==N:
+  print(N)
+elif N==a[::-1]:
   print("-1")
 else:
-	s=tuple(s)
-	for i in k:
-		l.append(i)
-	for i in l:
-		if i>s:
-			m=i
-			break
+    N=tuple(N)
+    for i in k:
+        l.append(i)
+    for i in l:
+        if i>N:
+            m=i
+            break
 
-	for i in l:
-		if i>s and i<m:
-			m=i
+    for i in l:
+        if i>N and i<m:
+            m=i
 
-	if m==-1:
-		print("-1")
-	else:
-		for i in m:
-			print(i,end="")
+    if m==-1:
+        print("-1")
+    else:
+        for i in m:
+            print(i,end="")
