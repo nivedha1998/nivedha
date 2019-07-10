52e9ec2194a37106b5e7536f73865e5def1a37f4
nivedha1998 <50647616+nivedha1998@users.noreply.github.com>
2019-07-10 17:56:49 +0530

    Update expo.py

--- expo.py
+++ expo.py
@@ -1,5 +1,5 @@
 import math
 N = int(input())
 k = int(input())
-power = math.pow(N,k)
-print("The Result of {0} Power {1} = {2}".format(N,k,power))
+P = math.pow(N,k)
+print("The Result of {0} Power {1} = {2}".format(N,k,P))
