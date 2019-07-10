917ccf2824fcf740bb3d1073bb0f2fb5c25e3f6a
nivedha1998 <50647616+nivedha1998@users.noreply.github.com>
2019-07-10 15:37:48 +0530

    Create H113.py

---
+++ H113.py
@@ -0,0 +1,12 @@
+N=int(input())
+if N==1:
+    print("YES")
+elif N%2!=0:
+    print("NO")
+else:
+    while N%2==0:
+        N=N//2
+    if N==1:
+        print("YES")
+    else:
+        print("NO")
