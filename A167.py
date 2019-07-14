798e69476b11198e298e26e7c9184a8b8a5e3a5b
nivedha1998 <50647616+nivedha1998@users.noreply.github.com>
2019-07-14 20:59:02 +0530

    Create A167.py

---
+++ A167.py
@@ -0,0 +1,12 @@
+N=input()
+var =len(N)
+if var > 1:
+   for i in range(2, var):
+       if (var % i) == 0:
+           print("no")
+           break
+   else:
+       print("yes")
+
+else:
+   print("no")
