9b1e151d97f24a3d5bcf01b5b159184f164b9952
nivedha1998 <50647616+nivedha1998@users.noreply.github.com>
2019-07-14 20:51:54 +0530

    Create A164.py

---
+++ A164.py
@@ -0,0 +1,10 @@
+N, k = list(map(int,input().split()))
+li = list(map(int,input().split()))
+if k in li:
+  print(k)
+else:
+  li.sort()
+  for i in range(N-1,-1,-1):
+    if li[i] < k:
+      print(li[i])
+      break
